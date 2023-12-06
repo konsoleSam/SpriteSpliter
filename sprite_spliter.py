8600870d1cbfc70c87946b0d963d079a172c9644
konsoleSam <109305646+konsoleSam@users.noreply.github.com>
2023-12-05 17:19:01 -0700

    Update sprite_spliter.py
    fixed a problem with the output file naming.
    The output file name included the original relative path, which threw some errors.

--- sprite_spliter.py
+++ sprite_spliter.py
@@ -16,7 +16,7 @@ def tile(filename, width,height):
     for i in range(0,frames):
         print(x,y)
         box = (x*sizex, y*sizey, (x+1)*sizex, (y+1)*sizey)
-        img.crop(box).save(os.path.join(ndir,str(i)+filename))
+        img.crop(box).save(os.path.join(ndir,str(i)+os.path.basename(filename)))
         x+=1
         if x>width-1:
             x=0
